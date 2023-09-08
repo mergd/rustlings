1a74eb387c3ad1ec995914421170c27073013b74
mergd <24722014+mergd@users.noreply.github.com>
2023-09-08 14:18:07 -0700

    Exercises

--- exercises/quiz3.rs
+++ exercises/quiz3.rs
@@ -13,19 +13,33 @@
 // to show that your changes allow alphabetical grades.
 
 // Execute `rustlings hint quiz3` or use the `hint` watch subcommand for a hint.
+use std::fmt;
 
-// I AM NOT DONE
-
+pub enum Grade {
+    Numeric(f32),
+    Letter(String),
+}
 pub struct ReportCard {
-    pub grade: f32,
+    pub grade: Grade,
     pub student_name: String,
     pub student_age: u8,
 }
 
+impl fmt::Display for Grade {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Grade::Numeric(n) => write!(f, "{}", n),
+            Grade::Letter(d) => write!(f, "{}", d),
+        }
+    }
+}
+
 impl ReportCard {
     pub fn print(&self) -> String {
-        format!("{} ({}) - achieved a grade of {}",
-            &self.student_name, &self.student_age, &self.grade)
+        format!(
+            "{} ({}) - achieved a grade of {}",
+            &self.student_name, &self.student_age, &self.grade
+        )
     }
 }
 
@@ -36,7 +50,7 @@ mod tests {
     #[test]
     fn generate_numeric_report_card() {
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Numeric(2.1),
             student_name: "Tom Wriggle".to_string(),
             student_age: 12,
         };
@@ -50,7 +64,7 @@ mod tests {
     fn generate_alphabetic_report_card() {
         // TODO: Make sure to change the grade here after you finish the exercise.
         let report_card = ReportCard {
-            grade: 2.1,
+            grade: Grade::Letter("A+".to_string()),
             student_name: "Gary Plotter".to_string(),
             student_age: 11,
         };
